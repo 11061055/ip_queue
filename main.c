635f72fa944c4c2468b99f454b2ba3c8fe6b1e5a
11061055 <buaashupan@gmail.com>
2019-09-29 10:44:10 +0800

    Update main.c

--- main.c
+++ main.c
@@ -7,23 +7,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 #include "libipq.h"
 
 #define BUFSIZE 2048
 
 struct tcp_pseudo_hdr
 {
-  unsigned int src;
-  unsigned int dst;
-  unsigned char zero;
-  unsigned char proto;
-  unsigned short length;
+  unsigned int 	  src;
+  unsigned int 	  dst;
+  unsigned char   zero;
+  unsigned char   proto;
+  unsigned short  length;
 };
 
 unsigned short csum (void *addr, int len)
 {
-  register unsigned short *idx = addr;
-  register unsigned int sum = 0;
+  register unsigned short* idx = addr;
+  register unsigned int    sum = 0;
 
   while (len > 1)
   {
@@ -32,12 +33,14 @@ unsigned short csum (void *addr, int len)
   }
 
   if (len == 1) {
+	  
     sum += htons (*(unsigned char *) idx << 8);
   }
 
-  sum = (sum >> 16) + (sum & 0xffff);
+  sum =  (sum >> 16) + (sum & 0xffff);
   sum += (sum >> 16);
-  sum = ~sum;
+  sum =  ~sum;
+	
   return (unsigned short)sum;
 }
 
@@ -46,34 +49,41 @@ unsigned short tcp_csum (struct iphdr *iph, struct tcphdr *tcph)
 {
   char tcp_csum_buf[BUFSIZE + sizeof(struct tcp_pseudo_hdr)];
 
-  int ip_total_len   = ntohs(iph->tot_len);
-  int ip_header_len  = iph->ihl * 4;
+  int ip_total_len   	= ntohs(iph->tot_len);
+  int ip_header_len  	= iph->ihl * 4;
 
-  int tcp_header_len = sizeof (struct tcphdr); // = 20
-  int tcp_option_len = tcph->doff * 4 - tcp_header_len;
-  int tcp_data_len   = ip_total_len - ip_header_len - tcp_header_len - tcp_option_len;
-  int tcp_total_len  = tcp_header_len + tcp_option_len + tcp_data_len;
+  int tcp_header_len 	= sizeof (struct tcphdr); // = 20
+  int tcp_option_len 	= tcph->doff * 4 - tcp_header_len;
+  int tcp_data_len   	= ip_total_len - ip_header_len - tcp_header_len - tcp_option_len;
+  int tcp_total_len  	= tcp_header_len + tcp_option_len + tcp_data_len;
 
   int pseudo_header_len = sizeof(struct tcp_pseudo_hdr);
 
   struct tcp_pseudo_hdr pseudo_hdr;
 
-  pseudo_hdr.src = iph->saddr;
-  pseudo_hdr.dst = iph->daddr;
-  pseudo_hdr.zero = 0;
-  pseudo_hdr.proto = IPPROTO_TCP;
-  pseudo_hdr.length = htons (tcp_total_len);
+  pseudo_hdr.src 	= iph->saddr;
+  pseudo_hdr.dst 	= iph->daddr;
+  pseudo_hdr.zero 	= 0;
+  pseudo_hdr.proto 	= IPPROTO_TCP;
+  pseudo_hdr.length 	= htons (tcp_total_len);
 
   int tcp_len_to_csum = sizeof (struct tcp_pseudo_hdr) + tcp_total_len;
 
-  memcpy (tcp_csum_buf, (unsigned char *)&pseudo_hdr, pseudo_header_len);
+  memcpy (tcp_csum_buf,
+	  (unsigned char *)&pseudo_hdr,
+	  pseudo_header_len);
+	
   memcpy (tcp_csum_buf + pseudo_header_len,
-	  (unsigned char *) tcph, tcp_header_len);
+	  (unsigned char *) tcph,
+	  tcp_header_len);
+	
   memcpy (tcp_csum_buf + pseudo_header_len + tcp_header_len,
 	  (unsigned char *) tcph + tcp_header_len,
 	  tcp_option_len);
+	
   memcpy (tcp_csum_buf + pseudo_header_len + tcp_header_len + tcp_option_len,
-	  (unsigned char *) tcph + tcp_header_len + tcp_option_len, tcp_data_len);
+	  (unsigned char *) tcph + tcp_header_len + tcp_option_len,
+	  tcp_data_len);
 
   return csum (tcp_csum_buf, tcp_len_to_csum);
 }
@@ -111,9 +121,10 @@ void main (int argc, char **argv)
       switch (ipq_message_type (buf))
       {
 	case NLMSG_ERROR:
+	{
 	  fprintf (stderr, "Received error message %d\n", ipq_get_msgerr (buf));
 	  break;
-
+	}
 	case IPQM_PACKET:
 	{
 	    ipq_packet_msg_t *msg = ipq_get_packet (buf);
@@ -151,16 +162,16 @@ void main (int argc, char **argv)
 		
 	    // 如上 目标地址 NAT 操作，将服务提供方由39.156.69.79改为111.202.103.60，且对应用透明
 
-	    tcph = (struct tcphdr *) (msg->payload + (iph->ihl << 2));
+	    tcph 	= (struct tcphdr *) (msg->payload + (iph->ihl << 2));
 
 	    tcph->check = 0;
 	    tcph->check = tcp_csum (iph, tcph);
 
-	    iph->check = 0;
-	    iph->check = csum (iph, iph->ihl * 4);
+	    iph->check 	= 0;
+	    iph->check 	= csum (iph, iph->ihl * 4);
 
-	    status = ipq_set_verdict (handle, msg->packet_id, NF_ACCEPT, msg->data_len, msg->payload);
-
+	    status 	= ipq_set_verdict (handle, msg->packet_id, NF_ACCEPT, msg->data_len, msg->payload);
+		
 	    if (status < 0)
 	    {
 	    	fprintf (stderr, "ipq set verdict error.\n");
@@ -168,12 +179,14 @@ void main (int argc, char **argv)
 	    }
 	    break;
  	}
-
 	default:
+	{
 	  fprintf (stderr, "Unknown message type!\n");
 	  break;
 	}
-
+		      
+      }// end switch
+	  
     } while (1);
 
     ipq_destroy_handle (handle);
